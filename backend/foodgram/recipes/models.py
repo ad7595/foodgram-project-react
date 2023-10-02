3beb33c4aa39a967a24a45092bc684eec57fdeda
ad7595 <degunovab@sibtrans.ru>
2023-10-03 01:12:34 +0700

    031023

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -14,7 +14,7 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название')
     measurement_unit = models.CharField(
         max_length=200, verbose_name='Единица измерения'
-        )
+    )
 
     class Meta:
         verbose_name = 'Ингредиент'
